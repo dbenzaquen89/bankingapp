ef4126d6f3d789bbbc33411db0eef09215143ea2
Daphne Benzaquen <98908807+dbenzaquen89@users.noreply.github.com>
2022-08-16 14:24:13 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
          <Routes>
             <Route path="/" exact element={<Home />} />
             <Route path='home' element={ <Home /> } />
-            <Route path='withdraw' element={ <Withdraw /> } />
+              <Route path='withdraw' element={ <Withdraw /> } />
             <Route path='deposit' element={ <Deposit /> } />
             <Route path='alldata' element={ <AllData /> } />
             <Route path='createaccount' element={ <CreateAccount /> } />
